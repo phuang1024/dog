1077729dcb062c8936683551e1433746ca05848f
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-06-18 18:21:26 -0700

    Added terminal color ANSI characters.

---
+++ src/utils.py
@@ -0,0 +1,28 @@
+#
+#  Dog
+#  Console printer with syntax highlighting.
+#  Copyright Patrick Huang 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+BLACK = "\x1b[30m"
+BLUE = "\x1b[34m"
+CYAN = "\x1b[36m"
+GREEN = "\x1b[32m"
+MAGENTA = "\x1b[35m"
+RED = "\x1b[31m"
+RESET = "\x1b[39m"
+WHITE = "\x1b[37m"
+YELLOW = "\x1b[33m"
