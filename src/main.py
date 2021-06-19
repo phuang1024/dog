479826be2bdb93fc82b1b0d0330dc010dd1343ad
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-06-18 18:26:05 -0700

    Added language detection.

---
+++ src/main.py
@@ -0,0 +1,66 @@
+#
+#  Dog
+#  Console printer with syntax highlighting.
+#  Copyright Patrick Huang 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import sys
+import io
+import argparse
+
+VERSION = "0.1.0"
+LANGS = (
+    "json",
+    "txt",
+)
+
+
+def get_lang(path, lang):
+    if lang != "":
+        return lang
+    if path == "-":
+        return "txt"
+
+    if path.endswith(".json"):
+        return "json"
+    if path.endswith(".txt"):
+        return "txt"
+
+    return "txt"
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Console printer with syntax highlighting.")
+    parser.add_argument("-v", "--version", help="Print version info.")
+    parser.add_argument("-l", "--lang", nargs="?", default="", help="Force language mode. Omit for autodetect.", choices=LANGS)
+    parser.add_argument("file", help="File path to read from (put - for stdin)")
+    args = parser.parse_args()
+
+    if args.version:
+        print(f"Dog {VERSION}")
+        return
+
+    if args.file == "-":
+        data = sys.stdin.read().encode()
+    else:
+        with open(args.file, "rb") as file:
+            data = file.read()
+
+    stream = io.BytesIO(data)
+    lang = get_lang(args.file, args.lang)
+
+
+main()
